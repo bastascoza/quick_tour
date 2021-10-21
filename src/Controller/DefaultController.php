3ce405bae643823b917e1a9409a5998586fb1f4d
aessordi <aessordi-ext@e-ambition.com>
2021-10-21 13:00:33 +0100

    getting familiar with php symfony

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,20 +4,34 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class DefaultController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
 {
-    
+    /**
+     * @Route("api/hello/{name}")
+     */
+    public function apiExample($name): Response
+    {
+        return $this->json([
+            'name' => $name,
+            'symfony' => 'rocks',
+        ]);
+    }
    /**
     * @Route("/hello/{name}")
     */
    public function index($name): Response
    {
-       return new Response("hello $name!");
+      // return new Response("hello $name!");
+       return $this->render('default/index.html.twig', [
+        'name' => $name,
+    ]);
    }
    /**
     * @Route("/simplicity")
     */
-   public function FunctionName(): Response
+   public function simple(): Response
    {
        return new Response("simple! nice! great!");
    }
