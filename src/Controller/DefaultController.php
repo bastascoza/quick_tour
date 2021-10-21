735ff7b72b090d04ba782acadb11571c3c138995
aessordi <aessordi-ext@e-ambition.com>
2021-10-21 18:09:46 +0100

    quick tour end

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 // src/Controller/DefaultController.php
 namespace App\Controller;
 
+use App\GreetingGenerator;
+//use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,13 +23,17 @@ class DefaultController extends AbstractController
    /**
     * @Route("/hello/{name}")
     */
-   public function index($name): Response
+   public function index($name,GreetingGenerator $generator): Response
    {
       // return new Response("hello $name!");
+      $greeting = $generator->getRandomGreeting();
+     // $Logger->info("saying $greeting to my $name!");
        return $this->render('default/index.html.twig', [
         'name' => $name,
+        'greeting'=>$greeting
     ]);
    }
+   
    /**
     * @Route("/simplicity")
     */
